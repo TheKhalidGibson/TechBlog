053da8ce338a71b1b05be153fcafd12605ab1a41
Khalid G <kgibson4795@gmail.com>
2023-09-04 19:53:38 -0400

    required posts and comments routes in index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const postsRoutes = require('./postsRoutes');
+const commentsRoutes = require('./commentsRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/posts', postsRoutes);
+router.use('/comments', commentsRoutes);
 
 module.exports = router;
