0aea10cc9a2370669edeadf3ba81e53356395235
Khalid G <kgibson4795@gmail.com>
2023-09-05 21:06:41 -0400

    added new relationships for Comments

--- models/index.js
+++ models/index.js
@@ -15,4 +15,16 @@ Comments.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
+User.hasMany(Comments, {
+  foreignKey: 'user_id'
+});
+
+Comments.belongsTo(Posts, {
+  foreignKey: 'post_id'
+});
+
+Posts.hasMany(Comments, {
+  foreignKey: 'post_id'
+});
+
 module.exports = { User, Posts, Comments };
