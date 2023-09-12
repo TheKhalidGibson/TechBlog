9c6a64b02a827f40be57a0d46f13f764b887e9ef
Khalid G <kgibson4795@gmail.com>
2023-09-12 09:58:32 -0400

    no editpostroutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postsRoutes = require('./postsRoutes');
 const commentsRoutes = require('./commentsRoutes');
-const editPostRoutes = require('./editPostRoutes');
+
 
 router.use('/users', userRoutes);
 router.use('/posts', postsRoutes);
 router.use('/comments', commentsRoutes);
-router.use('/editPost', editPostRoutes);
+
 
 module.exports = router;
